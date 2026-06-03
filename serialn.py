ce2dd9393062dfbe32ed2932ee59c7c4ca761c92
occult-frog <mhuzaifaa01234@gmail.com>
2026-06-03 18:03:14 +0300

    Update "show_recipes" command
    Updated the "show_recipes" command to create a "recipeindex.txt" file if not already present in the recipes folder and then append "hello" to it.

--- serialn.py
+++ serialn.py
@@ -8,9 +8,25 @@ def hi():
 @click.command()
 def show_recipes():
     recipefolder = Path.home() / "recipes"
+    recipeindexpath = recipefolder / "recipeindex.txt"
+
     if recipefolder.exists():
-        click.echo("yup its there")
+        click.echo("yup the recipes folder is there")
     else:
         Path.mkdir(recipefolder)
-        click.echo("folder mkdired")
+        click.echo("recipes folder mkdired")
+
+    if recipeindexpath.exists():
+        recipeindex = open(f"{recipefolder}/recipeindex.txt", "r+")
+        click.echo("can confirm the recipeindex file is there")
+    else:
+        recipeindex = open(f"{recipefolder}/recipeindex.txt", "x")
+        click.echo("recipeindex file has been created")
+
+    recipeindex.close()
+    recipeindex = open(f"{recipefolder}/recipeindex.txt", "a")
+
+    recipeindex.write("hello")
+    recipeindex.close()
+
     click.echo()
