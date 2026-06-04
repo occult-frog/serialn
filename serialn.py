23727b675cc219517e893b0b53cb51df9c10282f
occult-frog <mhuzaifaa01234@gmail.com>
2026-06-04 16:50:42 +0300

    Update "show_recipes" command and add new commands
    Renamed the "show_recipes" command to "list_recipes". "list_recipes" now lists all recipes from the recipeindex.txt file. Added 2 new commands -- "view_recipe" and "refresh_recipes". "view_recipe" shows the given recipe file's content in the terminal. "refresh_recipes" for now doesn't do anything

--- serialn.py
+++ serialn.py
@@ -1,3 +1,5 @@
+from idlelib import filelist
+
 import click
 from pathlib import Path
 
@@ -6,33 +8,62 @@ def hi():
     click.echo("hi")
 
 @click.command()
-def show_recipes():
+def list_recipes():
     recipefolder = Path.home() / "recipes"
     recipeindexpath = recipefolder / "recipeindex.txt"
-    recipelist = list(file.name for file in recipefolder.glob("*.txt"))
 
     if recipefolder.exists():
-        click.echo("yup the recipes folder is there")
+        click.echo("yup the recipes folder is there :D")
     else:
         Path.mkdir(recipefolder)
-        click.echo("recipes folder mkdired")
+        click.echo("recipes folder mkdired :P")
 
     if recipeindexpath.exists():
         recipeindex = open(f"{recipefolder}/recipeindex.txt", "r+")
-        click.echo("can confirm the recipeindex file is there")
+        click.echo("can confirm the recipeindex file is there :P\n")
     else:
         recipeindex = open(f"{recipefolder}/recipeindex.txt", "x")
-        click.echo("recipeindex file has been created")
+        click.echo("recipeindex file has been created :)\n")
 
     recipeindex.close()
-    recipeindex = open(f"{recipefolder}/recipeindex.txt", "a")
+    recipeindex = open(f"{recipefolder}/recipeindex.txt", "r")
+    templist = recipeindex.readlines()
+    filelist = []
 
-    for i in recipelist:
-        if i != "recipeindex.txt":
-            recipe = open(f"{recipefolder}/{i}", "r")
-            click.echo(f"{i}: {recipe.read()}")
+    for i in templist:
+        filelist.append(i.strip())
 
-    recipeindex.write("hello")
+    for i in filelist:
+        if (recipefolder/i).exists():
+            click.echo(f"{i}: found :)")
+        else:
+            click.echo(f"{i}: not found :(")
+
     recipeindex.close()
 
-    click.echo()
+
+@click.command()
+@click.argument('recipename')
+def view_recipe(recipename):
+    recipefolder = Path.home()/"recipes"
+
+    if recipefolder.exists() and (recipefolder/"recipeindex.txt").exists():
+        if (recipefolder/recipename).exists():
+            recipefile = open(recipefolder/recipename, "r")
+            click.echo(recipefile.read())
+    else:
+        click.echo("recipes folder or recipeindex file does not exist :(\n"
+                   "run \"show_recipes\" to create them")
+
+
+@click.command()
+@click.option('--remove_absent', 'remove_absent', help="remove recipes from the recipe index that aren't in the recipe folder")
+def refresh_recipes(remove_absent):
+    recipefolder = Path.home()/"recipes"
+    if recipefolder.exists() and (recipefolder/"recipeindex.txt").exists():
+        if remove_absent == "":
+            recipeindex = open(f"{recipefolder}/recipeidex.txt", "r+")
+            templist = recipeindex.readlines()
+            filelist = []
+            for i in templist:
+                filelist.append(i.strip())
