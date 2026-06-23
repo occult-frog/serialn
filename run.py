381bdfea0f007dbb13717f5b63d3bfebd42a8ed9
occult-frog <mhuzaifaa01234@gmail.com>
2026-06-23 22:23:36 +0300

    Add a run.py
    Added a run.py file to package serialn into an application.

---
+++ run.py
@@ -0,0 +1,17 @@
+import click
+from serialn import *
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(list_recipes, name="list-recipes")
+cli.add_command(view_recipe, name="view-recipe")
+cli.add_command(refresh_recipes, name="refresh-recipes")
+cli.add_command(add_recipe, name="add-recipe")
+cli.add_command(remove_recipe, name="remove-recipe")
+cli.add_command(search_tags, name="search-tags")
+cli.add_command(add_tags, name="add-tags")
+
+if __name__ == "__main__":
+    cli()
